4e9e359bccb8027f2f721f10fcd5a693505e0b87
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-16 21:22:02 +0100

    test4

--- client/src/api/RestaurantFinder.js
+++ client/src/api/RestaurantFinder.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    "postgres://soodckkrslmhrv:a19ddb19db1c542090a6bb96d5e861b97d984df301bb1b029cb7b4c4b637d04a@ec2-52-73-184-24.compute-1.amazonaws.com:5432/dbjjmastgb68bc/api/v1/restaurants",
+  baseURL: "http://localhost:3006/api/v1/restaurants",
 });
