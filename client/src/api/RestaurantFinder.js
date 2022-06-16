7d046fed9d003db378a03a2a8a856eb5cc497513
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-16 20:57:23 +0100

    test2

--- client/src/api/RestaurantFinder.js
+++ client/src/api/RestaurantFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3006/api/v1/restaurants",
+  baseURL: "https://https://yelptester.herokuapp.com/api/v1/restaurants",
 });
