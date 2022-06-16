072776f5a7a1329f179a7025507ec1979efe15f3
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-16 21:10:12 +0100

    test3

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ require("dotenv").config();
 
 const cors = require("cors");
 const db = require("./db");
+const path = require("path");
 const express = require("express");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 const PORT = process.env.PORT || 3001;
 
