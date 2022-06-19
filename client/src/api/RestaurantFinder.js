fae4780bd50b1c0230e1f052b9c2adf9433956d6
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-19 23:19:58 +0100

    build9933131

--- client/src/api/RestaurantFinder.js
+++ client/src/api/RestaurantFinder.js
@@ -4,3 +4,8 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://yelptester.herokuapp.com/api/v1/restaurants",
 });
+
+/* export default axios.create({
+  baseURL: `http://localhost:3006/api/v1/restaurants`,
+});
+ */
--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
